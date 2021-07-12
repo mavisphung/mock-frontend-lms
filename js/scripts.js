954f23a5ec595eaa901f86c95ee5ed12bbd1b3a2
mavisphung <huypc2410@gmail.com>
2021-07-12 13:40:01 +0700

    Thêm course-detail và nút log out tại trang index

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//$('.dropdown-toggle').dropdown();
+
+// const changeWidth = function() {
+//   const dropdownMenu = $('.btn-width');
+//   const btnDropdownToggle = $('.dropdown-toggle');
+//   dropdownMenu.width = btnDropdownToggle.width;
+// }
+// changeWidth();
