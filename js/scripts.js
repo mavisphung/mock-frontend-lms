789bc78c27fb6068aaf75510192775ee6d1909c3
mavisphung <huypc2410@gmail.com>
2021-07-22 10:29:10 +0700

    change script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,45 @@
-//$('.dropdown-toggle').dropdown();
+var dataTable;
 
-// const changeWidth = function() {
-//   const dropdownMenu = $('.btn-width');
-//   const btnDropdownToggle = $('.dropdown-toggle');
-//   dropdownMenu.width = btnDropdownToggle.width;
-// }
-// changeWidth();
+$(document).ready(function() {
+  dataTable = $('#myTable').DataTable({
+    "ajax": {
+      "url": "/admin/users/"
+    },
+    "columns" : [
+
+      {"data": "id"},
+      {"data": "username"},
+      {"data": "firstName"},
+      {"data": "lastName"},
+      {"data": "email"},
+      {"data": "address"},
+      {"data": "roles"},
+      {"data": "username"},
+      {
+        "data": "username",
+        "render": function(data) {
+          return `
+            <form th:action="@{/admin/enable}" method="post">
+							<input hidden th:name="username" th:value="${user.username}">
+							<span th:if="${user.enabled}" class="me-1">
+								<button class="btn btn-secondary">
+									<i class="fa fa-unlock"></i>
+								</button>
+							</span> <span th:if="${!user.enabled}" class="me-1">
+								<button class="btn btn-warning">
+									<i class="fa fa-lock"></i>
+								</button>
+							</span>
+						</form>
+						<form th:action="@{/admin/delete}" method="post">
+							<input hidden th:name="username" th:value="${user.username}">
+							<button class="btn btn-danger">
+								<i class="fa fa-trash"></i>
+							</button>
+						</form>
+          `;
+        }
+      }
+    ]
+  });
+});
